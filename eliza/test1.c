23008056a030855f402fe717a4c0c845685f5d10
torres-twr <rodrigomsfctorres@gmail.com>
2026-03-13 18:14:35 +0000

    Update test1.c

--- eliza/test1.c
+++ eliza/test1.c
@@ -1,24 +1,27 @@
-/*O objetivo desta parte inicial do codigo e de abrir o ficheiro de forma dinamica e ser
-capaz de o ler*/
+/*Rdrigo Torres- ist1117968
+Diogo Pinto - ist1117735
+Grupo 121
+LEEC - 25/26*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 #include <stdbool.h>
-char *getRest(char **info, char *dataBase, int count);
+//char *getRest(char **info, char *dataBase, int count);
+//char **readFile(        );
 void removeSymbols(char * vector);
 int searcher(char **info, char **dataBase, int count);
 char *round_robin(char **dataBase, int count, int*responsesBlock, int Block, int *crr);
 int main()
 {   //open the file and read it
     FILE *pfile = fopen("eliza.dat", "r");
-    char buffer[1024] = {0};
+    char buffer[1024] = {0}; //mete tudo a 0
     int capacity_keywords = 10;
     int capacity_responses = 10;
-    char **pinitial_3 = calloc(12, sizeof(char*));
+    char **pinitial_3 = calloc(12, sizeof(char*)); //como vou ignorar algum data ent meto tudo NULL para depois se der problema souber que é daqui
     char **pkey_words = malloc(capacity_keywords * sizeof(char*));
     char **presponses = malloc(capacity_responses * sizeof(char*));
-    char **pespecial = calloc(1, sizeof(char*));
+    //char **pespecial = calloc(1, sizeof(char*));
     if(pfile == NULL){
         printf("Could not open file\n");
         return 1;
@@ -26,7 +29,7 @@ int main()
     /*vai salvar as informacoes vinda do ficheiro
     nos diferentes arrays de pointers*/
     //vai ver quantos blocos de palavras chaves existem ate pq esse numero vai ser igual ao de respostas e colocar num for loop
-    int blocks = -4; //começar a -4 para remover os ! das frases de acolhimento e do especial
+    int blocks = -3; //começar a -4 para remover os ! das frases de acolhimento e do especial
     while(fgets(buffer, sizeof(buffer), pfile) != NULL){
         buffer[strcspn(buffer, "\r\n")] = '\0';
         if(strcmp(buffer, "!") == 0) blocks++;
@@ -36,14 +39,15 @@ int main()
     //para as 3 especiais
     int count = 0;
     while(count < 12 && fgets(buffer, sizeof(buffer), pfile) != NULL){
+            //o if ignora tudo e que nao sao as palavras de acolhimento
             if(count != 0 || count != 1 || count != 3 || count != 4 || count != 5 || count != 7 || count != 8 || count != 9 || count != 11){
-            buffer[strcspn(buffer, "\n")] = '\0'; //limpa o buff pode ser que mude para outra forma de limpar o buff
+            buffer[strcspn(buffer, "\r\n")] = '\0'; //limpa o buff pode ser que mude para outra forma de limpar o buff
             pinitial_3[count] = strdup(buffer); //copia o que se encontra no buffer para o array alocando automaticamente a memoria suficiente
             for(int i = 0; pinitial_3[count][i] != '\0'; i++){ //tranformar letra por letra em upper case para ter tudo maiusculo
                 pinitial_3[count][i] = toupper(pinitial_3[count][i]);
             }
             count++;
-            }
+            } else count++;
     }
     int count_keywords = 0;
     int *pkey_words_block = malloc(blocks * sizeof(int));
@@ -83,19 +87,14 @@ int main()
                 count_responses++;
         }
     }
-    /*diogo se estas a ler isto é so para saberes que eu estava a pensar que o melhor metodo seria colocar o pespecial no presponses e depois caso
-    nao haja match aquilo devolve o count_keyword que vai corresponder a ultima palavra chave que seria o nofound
-    e logo ai era so ativar a funcao round-robin para dar a resposta a variar do pespecial mas faz como achares melhor
-    que ainda nao sei muito bem como faria isto*/
-    while(fgets(buffer, sizeof(buffer), pfile) != NULL){
+    /*while(fgets(buffer, sizeof(buffer), pfile) != NULL){
             buffer[strcspn(buffer, "\r\n")] = '\0';
             pespecial[0] = strdup(buffer);
             for(int i = 0; pespecial[0][i] != '\0'; i++){
                 pespecial[0][i] = toupper(pespecial[0][i]);
             }
-    }
+    }*/
     fclose(pfile);
-    //pode ser preciso tratar dos arrays do file para remover tbm pontuação pelo menos para keyword******
     /*tudo em relação a ler o ficheiro esta feito
     hora de trabalhar com o stdin*/
     //int * counter_blocks = calloc(blocks, sizeof(int*)); //serve para saber em qual rotação estamos em cada bloco existente e começa em 0
@@ -110,10 +109,18 @@ int main()
                 buffer[i] = toupper(buffer[i]);
             }
             if(strcmp(buffer, "BYE") == 0){ //depois vamos ter de estudar o que fazer em relação ao adeus
-            printf("%s", pinitial_3[10]);
+            printf("%s\n", pinitial_3[10]);
             break;
             }
             removeSymbols(buffer);
+            bool isNothing = true; //podera ser adicicinado dentro de removeSymbols e serve para ver se é apenas espaços ou nao
+            for(int i = 0; buffer[i] != '\0'; i++){
+                if(buffer[i] != ' '){
+                    isNothing = false;
+                    break;
+                }
+            }
+            if(isNothing) continue;
             pinput[0] = strdup(buffer);
             //criar condição para caso de repeticao de input
             if(pinput[1] != NULL && strcmp(pinput[0], pinput[1]) == 0){ //caso o ultimo input seja igual ao input atual
@@ -125,10 +132,8 @@ int main()
             pinput[1] = strdup(pinput[0]); //guarda o input atual
 
             int match = searcher(pinput, pkey_words, count_keywords); //vai receber o index da keyword onde foi encontrado a correspondencia
-            if(match == count_keywords){
-               printf("%s\n", pespecial);
-            }else{
-                int currentBlock = pkey_words_block[match]; //obter o index do block onde esta a correspondencia
+            if(match == -1) match = count_keywords - 1; //vamos pegar  no ultima string de todas dentro do array de keywords para obtermos o index do bloco
+            int currentBlock = pkey_words_block[match]; //obter o index do block onde esta a correspondencia
                 //char *finishResponses = getRest(pinput, pkey_words[match], count_keywords);
                 /*if(finishResponses != NULL){
                 printf("%s %s\n", round_robin(presponses, count_responses, presponses_block, currentBlock, counterRound_robin), finishResponses);
@@ -137,7 +142,7 @@ int main()
                 char *resp = round_robin(presponses, count_responses, presponses_block, currentBlock, counterRound_robin);
                 printf("%s\n", resp);
                 free(resp);
-            }
+
         free(pinput[0]); //limpagem de memoria do stdin
     }
     //limpagem de memoria do stdin
@@ -151,8 +156,8 @@ int main()
     free(pkey_words);
     for(int i = 0; i < count_responses; i++) free(presponses[i]);
     free(presponses);
-    free(pespecial[0]);
-    free(pespecial);
+    //free(pespecial[0]);
+    //free(pespecial);
     free(pkey_words_block);
     free(presponses_block);
 
@@ -163,9 +168,17 @@ int main()
 void removeSymbols(char * vector){
     int i = 0;
     int j = 0;
+    bool repeatedSpaces = false;
     while(vector[i]){ //loop funciona ate encontrar o \0 porque este de todos os outros é o que possui o valor de 0 no ASCII
-        if(isalnum(vector[i]) || vector[i] == ' '){ //isalnum da o valor de 1 caso encontre um numero que nao é alphanumerico
+        if(isalnum(vector[i])){ //isalnum da o valor de 0 caso encontre algo que nao e alphanumerico
                 vector[j++] = vector[i]; //j++ nao significa que começa no 1 mas sim que começa no zero e sobe logo de seguida para 1
+                repeatedSpaces = false;
+        }
+        if(vector[i] == ' '){ //caso seja um espaço temos de ver pq queremos manter os espaços mas remover os repetidos
+            if(!repeatedSpaces){ //o ultimo char era uma letra ou numero qualquer
+                vector[j++] = vector[i];
+                repeatedSpaces = true; //comeca a ser verdade para assim no caso de haver outro espaco seguido ele nao vai ser colocado na string
+            }
         }
         i++;
     }
